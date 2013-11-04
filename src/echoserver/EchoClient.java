d62062518eeaa220bfd6a34a1962d41ec3d409ab
David Andrew Pagels <pagel093@axiom.morris.umn.edu>
2013-11-04 15:51:14 -0600

    done with Lab7

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,74 @@
 package echoserver;
 
-public class EchoClient {
+import java.net.*;
+import java.io.*;
+
+public class EchoClient implements Runnable {
+	public static Socket socket;
+	private static OutputStream output;
+	private static InputStream input;
+	private int flag;
+
+	private EchoClient(int flag){
+		this.flag = flag;
+	}
+
+	public static void main(String [] args) throws InterruptedException {
+		try{
+			String host = "localhost";
+			if(args.length != 0)
+				host = args[0];
+
+			socket = new Socket(host, 6013);
+			output = socket.getOutputStream();
+			input = socket.getInputStream();
+			Thread [] servT = new Thread[2];
+			
+			for(int i = 0; i < 2; i++){
+				servT[i] = new Thread(new EchoClient(i));
+				servT[i].start();
+			}
+
+			for(int i = 0; i < 2; i++)
+				servT[i].join();
+
+			socket.close();
+		}catch(IOException ioe){
+			System.out.println("IOException");
+			System.err.println(ioe);
+		}
+	}
+
+	@Override
+	public void run() {
+		if(flag == 0)
+			threadedInput();
+		else 
+			threadedOutput();
+
+	}
+
+	public void threadedInput(){
+		int byt;
+		try {
+			while((byt = System.in.read()) != -1){
+				output.write(byt);
+			}
+			socket.shutdownOutput();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void threadedOutput(){
+		try {
+			int byt;
+			while((byt = input.read()) != -1)
+				System.out.write(byt);
+			System.out.flush();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,59 @@
 package echoserver;
 
-public class EchoServer {
+import java.net.*;
+import java.util.ArrayList;
+import java.io.*;
+
+public class EchoServer implements Runnable{
+	Socket socket;
+	InputStream input;
+	OutputStream output;
+
+	private EchoServer(Socket client){
+		try {
+			socket = client;
+			input = client.getInputStream();
+			output = client.getOutputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String [] args){
+		int index = 0;
+		try{
+			ServerSocket sock = new ServerSocket(6013);
+			ArrayList<Thread> threads = new ArrayList<Thread>();
+
+			while(true){
+				while(index < 40){
+					Socket client = sock.accept();
+					threads.add(new Thread(new EchoServer(client)));
+					threads.get(index).start();
+					index++;
+				}	
+			}
+		} catch(IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
+
+	public void run(){
+		process();
+	}
+
+	public void process(){
+		int byt;
+		try {
+			while((byt = input.read()) != -1)
+				output.write(byt);
+			output.flush();
+			socket.shutdownOutput();
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
